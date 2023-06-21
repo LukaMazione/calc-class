c852f3153feb5099c19ab0c4189471c777b44319
Luka Mazione <luka.mazione@gmail.com>
2023-06-21 23:11:25 +0100

    calc info

---
+++ script.js
@@ -0,0 +1,6 @@
+// Stwórz kalkulator, który potrafi dodawać, odejmować, mnożyć i dzielić. Kalkulatory tym razem jest oparty na klasie.
+// ⦁ Klasa w konstruktorze otrzymuje liczbę a i liczbę b. Posiada również metody: 72 add, subtract, multiply, divide. Zwracają one odpowiednie dane.
+// ⦁ Dołóż do konstruktora walidację – sprawdzenie czy podane dane to liczby. Wyrzuć błąd, jeżeli nie.
+// ⦁ Do metody dzielenia również dołóż sprawdzenie czy liczba b nie jest 0.
+// ⦁ Przyjmij przez prompt dwie liczby. Stwórz nowy obiekt na bazie klasy i przekaż jej liczby użytkownika. Wykonaj wszystkie działania i wyświetl je w konsoli.
+//     Użyć try...catch, aby złapać we. Błędy i wyświetlić użytkownikowi.
